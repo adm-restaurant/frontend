42071f58906cc987d4304ceda6d4957d68474c00
Gabriel Vitorino <gabrielmiguel651@gmail.com>
2023-02-02 16:11:27 -0300

    Projeto Make your burger, feito em Vue.js com API's Json server e router integradas.

---
+++ src/components/Footer.vue
@@ -0,0 +1,29 @@
+<template>
+    <footer id="footer">
+        <p>Make Your Burger &copy; 2023</p>
+    </footer>
+</template>
+
+<script>
+export default{
+    name: "Footer"
+}
+</script>
+
+
+<style scoped>
+    #footer{
+        width: 100%;
+        height: 200px;
+        background-color: #222;
+        border-top: 4px solid #111;
+        color: #FCBA03;
+        display:flex;
+        align-items: center;
+        justify-content: center;
+        bottom: 0;
+        left: 0;
+        position:static;
+        padding: 15px 50px;
+    }
+</style>
